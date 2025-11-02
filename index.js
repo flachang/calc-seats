1f0c2e93a63af662b357a462d6bd1ee12c91606e
fls <zhangza46585@gmail.com>
2025-11-02 10:20:08 +0800

    ADD: code format

--- index.js
+++ index.js
@@ -8,34 +8,38 @@ const toLearn = ['react', 'vue', 'nodejs', 'webpack']
 const TextComponent = () => <div><div>zxx</div>hello, i am function component</div>
 
 class Home extends React.PureComponent {
-    renderFoot = () => <div>i am foot</div>
-    render() {
-        return <div style={{ marginTop: '100px' }}>
-            {/* 元素类型 */}
-            <div>hello world</div>
-            {/* fragment */}
-            <React.Fragment>
-                <div>fragment</div>
-            </React.Fragment>
-            {/* text */}
-            xiao mao
-            {/* array node */}
-            {toLearn.map(l => <div key={l}>let use learn {l}</div>)}
-            {/* 组件类型 */}
-            <TextComponent />
-
-            {this.renderFoot()}
-
-            <CloneEle />
-        </div>
-    }
+  renderFoot = () => <div>i am foot</div>;
+  render() {
+    return (
+      <div style={{ marginTop: "100px" }}>
+        {/* 元素类型 */}
+        <div>hello world</div>
+        {/* fragment */}
+        <React.Fragment>
+          <div>fragment</div>
+        </React.Fragment>
+        {/* text */}
+        xiao mao
+        {/* array node */}
+        {toLearn.map((l) => (
+          <div key={l}>let use learn {l}</div>
+        ))}
+        {/* 组件类型 */}
+        <TextComponent />
+        {this.renderFoot()}
+        <CloneEle />
+      </div>
+    );
+  }
 }
 
 // console.log(React.Children.map(Home().props.children, i => i))
 
-// jsx => React.createElement 
-// React.createElement 会 return 一个对象，就是 ReactElement 
-console.log(new < Home />.type)
-console.log(<div>1111</div>)
+// jsx => React.createElement
+// React.createElement 会 return 一个对象，就是 ReactElement
+console.log(new (<Home />).type());
+console.log(<div>1111</div>);
+
+
 
 reactDom.render(<Home />, document.getElementById("root"));
--- src/CloneEle.js
+++ src/CloneEle.js
@@ -1,16 +1,17 @@
 import React from "react";
 
 const Father = ({ children }) => {
+  const newChildren = React.cloneElement(children, { age: 18 });
 
-    const newChildren = React.cloneElement(children, { age: 18 })
-
-    return <div>{newChildren}</div>
-}
+  return <div>{newChildren}</div>;
+};
 
 const Son = ({ age }) => <div>{` 我永远${age}岁 `}</div>
 
 export default function () {
-    return <Father>
-        <Son />
-    </Father>
+return (
+  <Father>
+    <Son />
+  </Father>
+);
 }
